495e1466dce9005889763cf8db5a456ca8b0b6d3
Priya <priyakamani92@gmail.com>
2021-11-22 11:15:57 +0530

    VIDEO AND COMMENT SECTIONS UPDATED

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+var carouselWidth = $(".comments")[0].scrollWidth;
+var cardWidth = $(".comment").width();
+var scrollPosition = 0;
+$(".carousel-control-next").on("click", function () {
+  if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+    scrollPosition += cardWidth;  //update scroll position
+    $(".comments").animate({ scrollLeft: scrollPosition },600); //scroll left
+  }
+});
+$(".carousel-control-prev").on("click", function () {
+  if (scrollPosition > 0) {
+    scrollPosition -= cardWidth;
+    $(".comments").animate(
+      { scrollLeft: scrollPosition },
+      600
+    );
+  }
+});
+
+var multipleCardCarousel = document.querySelector(
+  "#commentsControls"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  //rest of the code
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide");
+}
